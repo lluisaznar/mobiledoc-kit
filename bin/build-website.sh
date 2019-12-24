6c9bacd12275c61b42eb935ec84addefa9c41424
Lluís Aznar <lluisaznar@fastmail.com>
2019-12-24 18:28:44 +0100

    [SH] build-website -> don't use git

--- bin/build-website.sh
+++ bin/build-website.sh
@@ -11,7 +11,7 @@ echo "Copying $(pwd)/docs to $(pwd)/website/demo/docs"
 mkdir -p website/demo/docs
 cp -R docs/* website/demo/docs/
 
-CURRENT_SHA="$(git rev-parse HEAD)"
-git add website/
-git commit -m "built website from $CURRENT_SHA"
-echo "Built and committed as $CURRENT_SHA"
+# CURRENT_SHA="$(git rev-parse HEAD)"
+# git add website/
+# git commit -m "built website from $CURRENT_SHA"
+# echo "Built and committed as $CURRENT_SHA"
